8a18b5bbad906856268a0238264f723977513f0b
Vlad Kashkarov <kwa-111@yandex.ru>
2024-09-05 23:57:56 +0300

    added test suite demoqa

---
+++ src/test/java/LessonWork.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class LessonWork {
+    @Test
+    void readTestForm() {
+        open("https://demoqa.com/automation-practice-form");
+        $("[id = firstName]").setValue("Vlad");
+        $("[id = lastName]").setValue("kashkarov");
+        $("[id = userEmail]").setValue("test@yandex.ru");
+        $("[id = userNumber]").setValue("89991111111");
+        $("[id = genterWrapper]").find(".custom-control-label").click();
+        $("[id = dateOfBirthInput]").sendKeys(Keys.CONTROL + "A");
+        $("[id = dateOfBirthInput]").sendKeys("6.12.1996");
+        $("[id = dateOfBirthInput]").pressEnter();
+        $("[id = subjectsInput]").setValue("Math");
+        $("[id = react-select-2-option-0]").click();
+        $("[id = subjectsInput]").setValue("English");
+        $("[id = react-select-2-option-0]").click();
+        $("[id = hobbiesWrapper]").find("[for=hobbies-checkbox-1]").click();
+        $("[id = hobbiesWrapper]").find("[for=hobbies-checkbox-2]").click();
+        $("[id = hobbiesWrapper]").find("[for=hobbies-checkbox-3]").click();
+        $("[id = uploadPicture]").uploadFile(new File("src/test/resources/tst.jpg"));
+        $("[id = currentAddress]").setValue("Krasnodar st. Severnaya 200 h.1");
+        $("[id = state]").click();
+        $("[id = react-select-3-option-0]").click();
+        $("[id = city]").click();
+        $("[id = react-select-4-option-0]").click();
+        Selenide.sleep(10000);
+        $("[id = submit]").click();
+    }
+
+}
