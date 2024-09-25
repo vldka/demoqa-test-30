baa71b2f298ce4e04e3b59f8eb5569a8b928b630
Vlad Kashkarov <kwa-111@yandex.ru>
2024-09-25 11:50:54 +0300

    fix: исправления по замечаниям

--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -3,39 +3,32 @@ package tests;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
+import utils.RandomUtils;
 
-import java.text.SimpleDateFormat;
 import java.util.Locale;
-
-import static utils.RandomUtils.getMonthBrithday;
 
 public class RegistrationWithPageObjectsTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
-
+    private final RandomUtils randomUtils = new RandomUtils();
     @Test
     void successfulRegistrationTest() {
-        Faker rufaker = new Faker(new Locale("ru"));
         Faker enfaker = new Faker(new Locale("en-US"));
         // Тестовые данные
-        var firstName = rufaker.name().firstName();
-        var lastName = rufaker.name().lastName();
-        var email = enfaker.internet().emailAddress();
-        var gender = enfaker.options().option("Male", "Female", "Other");
-        var phone = rufaker.phoneNumber().subscriberNumber(10);
-        var dayOfBirth = new SimpleDateFormat("d").format(enfaker.date().birthday());
-        var monthOfBirth = getMonthBrithday();
-        var yearOfBirth = new SimpleDateFormat("yyyy").format(enfaker.date().birthday());
-        var subjects = enfaker.options().option("Maths", "English");
-        var hobbies = enfaker.options().option("Sports", "Reading", "Music");
+        var firstName = randomUtils.getFirsttName();
+        var lastName = randomUtils.getLastName();
+        var email = randomUtils.getEmail();
+        var gender = randomUtils.getGender();
+        var phone = randomUtils.getPhone();
+        var dayOfBirth = randomUtils.getDayBrithday();
+        var monthOfBirth = randomUtils.getMonthBrithday();
+        var yearOfBirth = randomUtils.getYearBrithday();
+        var subjects = randomUtils.getSubjects();
+        var hobbies = randomUtils.getHobbies();
         var fileName = "tst.jpg";
-        var currentAddress = rufaker.address().fullAddress();
-        var state = enfaker.options().option("NCR", "Haryana");
-        var city = switch (state) {
-            case "NCR" -> enfaker.options().option("Delhi", "Gurgaon", "Noida");
-            case "Haryana" -> enfaker.options().option("Karnal", "Panipat");
-            default -> "";
-        };
+        var currentAddress = randomUtils.getAddress();
+        var state = randomUtils.getState();
+        var city = randomUtils.getCity(state);
         //Действия по заполнению
         registrationPage
                 .openPage()
--- src/test/java/utils/RandomUtils.java
+++ src/test/java/utils/RandomUtils.java
@@ -2,19 +2,82 @@ package utils;
 
 import com.github.javafaker.Faker;
 
+import java.text.SimpleDateFormat;
 import java.time.ZoneId;
+import java.util.Locale;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
-    static Faker enfaker = new Faker();
-
+     Faker enfaker = new Faker();
+     Faker rufaker = new Faker(new Locale("ru"));
     public static void main(String[] args) {
     }
 
-    public static String getMonthBrithday() {
-        //перевод даты в LocalDate
-        var month = enfaker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getMonth().toString();
-        // Верхний регистр только для первого элемента
-        return month.charAt(0) + month.substring(1).toLowerCase();
+    public String getMonthBrithday() {
+       return enfaker.options().option("January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December");
+    }
+
+    public String getDayBrithday() {
+
+        return String.format("%02d", enfaker.number().numberBetween(1,28));
+    }
+
+    public String getYearBrithday() {
+
+        return new SimpleDateFormat("yyyy").format(enfaker.date().birthday());
+    }
+
+    public String getPhone() {
+
+        return rufaker.phoneNumber().subscriberNumber(10);
+    }
+
+    public String getGender() {
+
+        return enfaker.options().option("Male", "Female", "Other");
+    }
+
+    public String getEmail() {
+
+        return enfaker.internet().emailAddress();
+    }
+
+    public String getAddress() {
+
+        return rufaker.address().fullAddress();
+    }
+    public String getLastName() {
+
+        return rufaker.name().lastName();
+    }
+
+    public String getFirsttName() {
+
+        return rufaker.name().firstName();
+    }
+
+    public String getState() {
+
+        return enfaker.options().option("NCR", "Haryana");
+    }
+
+    public String getCity(String state) {
+
+        return switch (state) {
+            case "NCR" -> enfaker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Haryana" -> enfaker.options().option("Karnal", "Panipat");
+            default -> "";
+        };
+    }
+
+    public String getSubjects() {
+
+        return enfaker.options().option("Maths", "English");
+    }
+
+    public String getHobbies() {
+
+        return enfaker.options().option("Sports", "Reading", "Music");
     }
 }
