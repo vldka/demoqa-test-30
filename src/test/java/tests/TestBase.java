08a4284271f34e2df4d2bf24ee284ed45630071a
Vlad Kashkarov <kwa-111@yandex.ru>
2024-11-18 18:01:45 +0300

    fix: запуск локального браузера на selenide

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -11,7 +11,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
